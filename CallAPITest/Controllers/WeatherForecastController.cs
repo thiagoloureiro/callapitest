a30d08158cc3dc43727e7075fe10e697949220db
Thiago Loureiro <thiago.loureiro@pl.abb.com>
2024-03-15 11:50:56 +0100

    test

---
+++ CallAPITest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CallAPITest.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public string Get(string url)
+    {
+        var client = new HttpClient();
+        var response = client.GetAsync(url).Result;
+        return response.Content.ReadAsStringAsync().Result;
+    }
+}
